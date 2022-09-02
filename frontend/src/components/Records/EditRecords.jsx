be8f0a794816456b973fd8d4502ed1dc75ceb9b2
wtffka <bal1337@yandex.ru>
2022-09-02 19:33:50 +0300

    try to deploy heroku

--- frontend/src/components/Records/EditRecords.jsx
+++ frontend/src/components/Records/EditRecords.jsx
@@ -1,42 +1,34 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
-import { selectors, actions as recordsActions } from '../../slices/recordsSlice.js';
 import routes from '../../routes.js';
 
 const EditRecords = () => {
   const { t } = useTranslation();
 
   const history = useHistory();
-  const dispatch = useDispatch();
 
-  const records = useSelector(selectors.selectAll);
-
-  const updateRecords = async (event) => {
-    event.preventDefault();
+  const updateRecords = async () => {
     try {
       await axios.put(routes.editAll());
-      dispatch(recordsActions.updateRecords(records));
       const from = { pathname: routes.recordsPagePath() };
-      history.push(from);
+      history.push(from, { message: 'dataUpdated' });
     } catch (e) {
-      const from = { pathname: routes.recordsPagePath() };
+      const from = { pathname: routes.homePagePath() };
       history.push(from);
     }
   };
 
   return (
     <>
-      <h2 className="my-4">{t('changeFormatNumber')}</h2>
-      <Form onSubmit={(event) => updateRecords(event)}>
-        <Button type="submit" variant="success" style={{ width: '130px', height: '50px' }}>{t('confirm')}</Button>
-      </Form>
-      <Button variant="secondary" style={{ width: '130px', height: '50px', marginTop: '10px' }}>
-        <Link className="nav-link text-white" to={routes.recordsPagePath()}>
+      <Button onClick={updateRecords} variant="primary" className="btn-sm" style={{ height: '50px' }}>
+        {t('confirm')}
+      </Button>
+      <Button variant="primary" className="btn-sm" style={{ height: '50px', marginLeft: '160px' }}>
+        <Link className="nav-link text-white" to={routes.homePagePath()}>
           {t('goBack')}
         </Link>
       </Button>
